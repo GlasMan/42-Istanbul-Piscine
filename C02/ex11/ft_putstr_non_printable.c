674674175f3fd458c04ad766230d46fc55719052
Salih burak Coskun <scoskun@k1m13s01.42istanbul.com.tr>
2022-04-19 14:22:31 +0300

    %85

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scoskun <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/30 03:47:20 by scoskun           #+#    #+#             */
+/*   Updated: 2021/10/30 04:27:25 by scoskun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 31 || str[i] == 127)
+		{
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[str[i] / 16]);
+			ft_putchar("0123456789abcdef"[str[i] % 16]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
+}
